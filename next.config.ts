065537a36c9901132257718c782cf5dffd370707
Vaibhav Singh <vaibhav.fullstack.dev@gmail.com>
2026-08-02 11:15:00 +0530

    feat: initialize Next.js 16 project with TypeScript and Tailwind CSS v4

---
+++ next.config.ts
@@ -0,0 +1,63 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import type { NextConfig } from "next";
+
+const currentDir = dirname(fileURLToPath(import.meta.url));
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: currentDir,
+  },
+  typescript: {
+    // ignoreBuildErrors: true, // ignore TypeScript errors during build
+  },
+  /* config options here */
+  images: {
+    // allow all images from the Pexels domain
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+        port: "",
+        pathname: "/photos/**",
+      },
+      // allow all images from the Unsplash domain
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/photo-**",
+      },
+      // allow all images from the Picsum domain
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
+      },
+      // allow all remote images from the Vercel Blob domain
+      {
+        protocol: "https",
+        hostname: "vercel.blob",
+        port: "",
+        pathname: "/**",
+      },
+      // allow all images from the Vercel Blob storage domain
+      {
+        protocol: "https",
+        hostname: "vercel.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
+      // allow all images from bvchjdsex1t3pg17.public.blob.vercel-storage.com
+      {
+        protocol: "https",
+        hostname: "bvchjdsex1t3pg17.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
